d25198a49e7be0ef16aad84cf9c31f638c55126c
geon-codder <akbarneo42@gmail.com>
2022-04-22 11:30:35 +0800

    a

--- app/src/main/java/com/geco/challangech5/RegisterFragment.kt
+++ app/src/main/java/com/geco/challangech5/RegisterFragment.kt
@@ -1,20 +1,64 @@
 package com.geco.challangech5
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.geco.challangech5.R
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.geco.challangech5.databinding.FragmentRegisterBinding
+
 
 class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sharedPreferences : SharedPreferences =
+            requireActivity().getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+
+        binding.btnRegist.setOnClickListener {
+            val username: String = binding.etUsernameRegist.text.toString()
+            val pass: String = binding.etPassRegist.text.toString()
+            val repass: String = binding.etRePassRegist.text.toString()
+
+            if (username.isNullOrEmpty() || pass.isNullOrEmpty() || repass.isNullOrEmpty()){
+                Toast.makeText(activity,"Isi data yang kosong", Toast.LENGTH_SHORT).show()
+            }else{
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                if(pass == repass){
+                    editor.putString("user", username)
+                    editor.putString("pass", pass)
+                    editor.apply()
+                    Toast.makeText(activity,"Registrasi Berhasil", Toast.LENGTH_SHORT).show()
+                    it.findNavController().navigateUp()
+                }else{
+                    Toast.makeText(activity,"Password tidak cocok", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/geco/challangech5/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/geco/challangech5/fragment/home/HomeFragment.kt
@@ -1,21 +1,37 @@
 package com.geco.challangech5.fragment.home
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.geco.challangech5.R
+import com.geco.challangech5.databinding.FragmentHomeBinding
+import com.geco.challangech5.databinding.FragmentRegisterBinding
 
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val sharedPreferences: SharedPreferences =
+            requireActivity().getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+
+        val username = sharedPreferences.getString("user", "defaultUser")
+        binding.tvWelcome.text = "Selamat Datang, $username"
     }
 
 }
